a0bbe6e84de07b7fbbab94aa1c7e6c43f60bb6f1
Michael L Perry <michael@qedcode.com>
2015-08-13 20:22:03 -0500

    Bare jinaga package.

---
+++ src/jinaga.js
@@ -0,0 +1,12 @@
+function fact(message) {
+
+}
+
+function query(start, clauses, resultAdded, resultRemoved) {
+
+}
+
+module.exports = {
+  fact: fact,
+  query: query
+};
--- test/querySpec.js
+++ test/querySpec.js
@@ -1,10 +1,33 @@
 var chai = require("chai");
+var j = require ('../src/jinaga')
 
 chai.should();
 
 describe("Query", function () {
   it("should return a matching message", function () {
-    var one = 1;
-    one.should.equal(0);
+    var chores = {
+      name: "Chores"
+    };
+
+    var trash = {
+      list: chores,
+      description: "Take out the trash"
+    };
+
+    function tasksInList(l) {
+      return {
+        list: l
+      };
+    }
+
+    var tasks = [];
+    function taskAdded(task) {
+      tasks.push(task);
+    }
+
+    j.query(chores, [tasksInList], taskAdded);
+    j.fact(trash);
+
+    tasks.length.should.equal(1);
   });
 });
