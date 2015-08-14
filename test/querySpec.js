3ffe3a1f2cbdad23fa695fb0c5bd058cfaca6138
Michael L Perry <michael@qedcode.com>
2015-08-13 19:26:04 -0500

    Added mocha tests.

---
+++ test/querySpec.js
@@ -0,0 +1,10 @@
+var chai = require("chai");
+
+chai.should();
+
+describe("Query", function () {
+  it("should return a matching message", function () {
+    var one = 1;
+    one.should.equal(0);
+  });
+});
