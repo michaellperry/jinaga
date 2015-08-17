ebf25817e252b2f269a67425adb79177ec265ee5
Michael L Perry <michael@qedcode.com>
2015-08-16 22:38:09 -0500

    First cut at the structure of a query.
    And the interface of a storage provider.

--- src/jinaga.ts
+++ src/jinaga.ts
@@ -1,11 +1,78 @@
-class Query {
-    constructor(public resultAdded: (message: Object) => void) {
+/*
+ * TODO: This file should be broken down once these ideas get more fleshed out.
+ * But for now, I wanted to see them all at once.
+ */
+
+enum Direction {
+    Predecessor,
+    Successor
+}
+
+enum Quantifier {
+    Exists,
+    NotExists
+}
+
+class Condition {
+    constructor() {
     }
+}
+
+class ExistentialCondition extends Condition {
+    constructor(
+        public quantifier: Quantifier,
+        public joins: Array<Join>) {
+        super();
+    }
+}
+
+class PropertyCondition extends Condition {
+    constructor(
+        public name: string,
+        public value: any) {
+        super();
+    }
+}
+
+class Join {
+    constructor(
+        public direction: Direction,
+        public role: string,
+        public conditions: Array<Condition>) {
+    }
+}
+
+class Inverse {
+    constructor(
+        public targets: Array<Join>,
+        public added: Array<Join>,
+        public removed: Array<Join>) {
+    }
+}
+
+class Query {
+    constructor(
+        public start: Object,
+        public joins: Array<Join>,
+        public resultAdded: (message: Object) => void,
+        public resultRemoved: (message: Object) => void,
+        public inverses: Array<Inverse>) {
+    }
+}
+
+interface StorageProvider {
+    executeQuery(
+        start: Object,
+        joins: Array<Join>,
+        result: (error: string, messages: Array<Object>) => void);
 }
 
 class Jinaga {
     private queries: Array<Query> = new Array<Query>();
     private messages: Object[] = [];
+
+    public save(storage: StorageProvider) {
+    }
 
     public fact(message: Object) {
         this.messages.push(message);
@@ -21,7 +88,12 @@ class Jinaga {
         resultAdded: (result: Object) => void,
         resultRemoved: (result: Object) => void) {
 
-        this.queries.push(new Query(resultAdded));
+        this.queries.push(new Query(
+            start,
+            [],
+            resultAdded,
+            resultRemoved,
+            []));
 
         for (var i = 0; i < this.messages.length; i++) {
             resultAdded(this.messages[i]);
