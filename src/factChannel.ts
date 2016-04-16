4c37e7f87471ccf952fbdcf5bc41d143a36a0206
Michael L Perry <michael@qedcode.com>
2016-04-15 23:10:38 -0500

    Serialize predecessors.

--- src/factChannel.ts
+++ src/factChannel.ts
@@ -1,14 +1,41 @@
+import Interface = require('./interface');
+import isPredecessor = Interface.isPredecessor;
+
+class FactReference {
+    constructor(
+        public id: number
+    ) {
+    }
+}
+
 class FactChannel {
+    private nextId: number;
+    
     constructor(
         private output: (string) => void
     ) {
+        this.nextId = 1;
     }
     
-    send(fact: Object) {
+    sendFact(fact: Object): FactReference {
+        var memento = {};
+        for (var field in fact) {
+            var value = fact[field];
+            if (isPredecessor(value)) {
+                memento[field] = this.sendFact(value);
+            }
+            else {
+                memento[field] = value;
+            }
+        }
         this.output(JSON.stringify({
-            id: 1,
-            fact: fact
+            type: 'fact',
+            id: this.nextId,
+            fact: memento
         }));
+        var reference = new FactReference(this.nextId);
+        this.nextId += 2;
+        return reference;
     }
 }
 
--- test/protocolSpec.js
+++ test/protocolSpec.js
@@ -5,17 +5,44 @@ var FactChannel = require('../node/factChannel');
 var should = chai.should();
 
 describe('protocol', function () {
-    it('should serialize one fact', function () {
-        var messages = [];
-        var channel = new FactChannel(function (message) {
+    var messages;
+    var channel;
+    
+    beforeEach(function () {
+        messages = [];
+        channel = new FactChannel(function (message) {
             messages.push(message);
         });
-        channel.send({
+    });
+    
+    it('should serialize one fact', function () {
+        channel.sendFact({
             type: "Jinaga.User",
             publicKey: "----BEGIN PUBLIC KEY---XXXXX"
         });
+        
         should.equal(1, messages.length);
-        should.equal('{"id":1,"fact":{"type":"Jinaga.User","publicKey":"----BEGIN PUBLIC KEY---XXXXX"}}',
+        should.equal(
+            '{"type":"fact","id":1,"fact":{"type":"Jinaga.User","publicKey":"----BEGIN PUBLIC KEY---XXXXX"}}',
             messages[0]);
+    });
+    
+    it('should serialize a predecessor', function () {
+        channel.sendFact({
+            type: "List",
+            name: "Chores",
+            from: {
+                type: "Jinaga.User",
+                publicKey: "----BEGIN PUBLIC KEY---XXXXX"
+            }
+        });
+        
+        should.equal(2, messages.length);
+        should.equal(
+            '{"type":"fact","id":1,"fact":{"type":"Jinaga.User","publicKey":"----BEGIN PUBLIC KEY---XXXXX"}}',
+            messages[0]);
+        should.equal(
+            '{"type":"fact","id":3,"fact":{"type":"List","name":"Chores","from":{"id":1}}}',
+            messages[1]);
     });
 });
