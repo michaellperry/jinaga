f396aa13ba6347f4640db6a43621dfbec7ee4797
Michael L Perry <michael@qedcode.com>
2016-05-23 21:19:02 -0500

    Bot client

---
+++ src/jinaga.bot.client.ts
@@ -0,0 +1,26 @@
+import Interface = require("./interface");
+import NetworkProvider = Interface.NetworkProvider;
+import Coordinator = Interface.Coordinator;
+import Query = Interface.Query;
+
+class JinagaBot implements NetworkProvider {
+    init(coordinator: Coordinator) {
+        
+    }
+    
+    watch(start: Object, query: Query) {
+        
+    }
+    
+    stopWatch(start: Object, query: Query) {
+        
+    }
+    
+    query(start: Object, query: Query, token: number) {
+        
+    }
+    
+    fact(fact: Object) {
+        
+    }
+}
