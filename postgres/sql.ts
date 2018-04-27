4594f93c8253a235e8c175748f8c4d91518eed30
Michael L Perry <michael@qedcode.com>
2018-04-27 14:02:08 -0500

    Moved to a functional state machine.

--- postgres/sql.ts
+++ postgres/sql.ts
@@ -20,21 +20,20 @@ type QueryParts = {
     whereClause: string
 };
 
+type State = {
+    parts: QueryParts,
+    transition: (step: Step) => State
+};
+
 class QueryBuilder {
     private parameters: any[] = [];
     private nextAlias: number = 1;
 
     sqlFromSteps(start: FactReference, steps: Step[]): QueryParts {
-        if (steps.length >= 1) {
-            const firstStep = steps[0];
-            if (firstStep instanceof Join) {
-                if (firstStep.direction === Direction.Successor) {
-                    return this.sqlFromSuccessor(start, firstStep.role, steps.slice(1));
-                }
-            }
-        }
-    
-        throw new Error("Cannot yet handle this query.");
+        const final = steps.reduce((state, step) => {
+            return state.transition(step);
+        }, this.stateStart(start));
+        return final.parts;
     }
     
     assembleQuery(parts: QueryParts): SqlQuery {
@@ -54,7 +53,24 @@ class QueryBuilder {
         return sql;
     }
 
-    private sqlFromSuccessor(start: FactReference, role: string, steps: Step[]): QueryParts {
+    private stateStart(start: FactReference): State {
+        return {
+            parts: null,
+            transition: (step: Step) => this.transitionFromStart(start, step)
+        };
+    }
+
+    private transitionFromStart(start: FactReference, step: Step): State {
+        if (step instanceof Join) {
+            if (step.direction === Direction.Successor) {
+                return this.handleStartSuccessor(start, step.role);
+            }
+        }
+    
+        throw new Error("Cannot yet handle this query.");
+    }
+
+    private handleStartSuccessor(start: FactReference, role: string): State {
         const alias = this.allocateAlias();
         const prefix = alias + '.successor_';
         const fromClause = 'FROM public.edge ' + alias;
@@ -62,49 +78,59 @@ class QueryBuilder {
         const whereClause = alias + '.predecessor_type = ' + this.addParameter(start.type) +
             ' AND ' + alias + '.predecessor_hash = ' + this.addParameter(start.hash) +
             ' AND ' + alias + '.role = ' + this.addParameter(role);
+        const parts = { prefix, fromClause, joins, whereClause };
 
-        return this.sqlWithConditions({ prefix, fromClause, joins, whereClause }, steps);
+        return this.stateMatched(parts);
     }
 
-    private sqlWithConditions(parts: QueryParts, steps: Step[]): QueryParts {
-        if (steps.length === 0) {
-            return parts;
+    private stateMatched(parts: QueryParts): State {
+        return {
+            parts,
+            transition: (step: Step) => this.transitionFromMatched(parts, step)
+        };
+    }
+
+    private transitionFromMatched(parts: QueryParts, step: Step): State {
+        if (step instanceof PropertyCondition && step.name === "type") {
+            return this.handleMatchedType(parts, step.value);
         }
-        else {
-            const nextStep = steps[0];
-            if (nextStep instanceof PropertyCondition && nextStep.name === "type") {
-                const whereClause = parts.whereClause +
-                    ' AND ' + parts.prefix + 'type = ' + this.addParameter(nextStep.value);
-                return this.sqlWithConditions({
-                    prefix: parts.prefix,
-                    fromClause: parts.fromClause,
-                    joins: parts.joins,
-                    whereClause
-                }, steps.slice(1));
-            }
-            else if (nextStep instanceof ExistentialCondition) {
-                const existence = nextStep.quantifier === Quantifier.Exists ? 'EXISTS' : 'NOT EXISTS';
-                const alias = this.allocateAlias();
-                const fromClause = 'FROM public.edge ' + alias;
-                const joins = parts.joins;
-                const prefix = alias + '.predecessor_';
-                const childWhereClause = prefix + 'type = ' + parts.prefix + 'type' +
-                    ' AND ' + prefix + 'hash = ' + parts.prefix + 'hash';
-                const childQuery = this.assembleChildQueryString(
-                    { prefix, fromClause, joins, whereClause: childWhereClause }
-                );
-                const whereClause = parts.whereClause +
-                    ' AND ' + existence + ' (' + childQuery + ')';
-                return this.sqlWithConditions({
-                    prefix: parts.prefix,
-                    fromClause: parts.fromClause,
-                    joins: parts.joins,
-                    whereClause
-                }, steps.slice(1));
-            }
+        else if (step instanceof ExistentialCondition) {
+            return this.handleMatchedExistentialCondition(parts, step.quantifier, step.steps);
         }
-    
+
         throw new Error("Cannot yet handle this query.");
+    }
+
+    private handleMatchedType(parts: QueryParts, type: string): State {
+        const whereClause = parts.whereClause +
+            ' AND ' + parts.prefix + 'type = ' + this.addParameter(type);
+        return this.stateMatched({
+            prefix: parts.prefix,
+            fromClause: parts.fromClause,
+            joins: parts.joins,
+            whereClause
+        });
+    }
+
+    private handleMatchedExistentialCondition(parts: QueryParts, quantifier: Quantifier, steps: Step[]): State {
+        const existence = quantifier === Quantifier.Exists ? 'EXISTS' : 'NOT EXISTS';
+        const alias = this.allocateAlias();
+        const fromClause = 'FROM public.edge ' + alias;
+        const joins = parts.joins;
+        const prefix = alias + '.predecessor_';
+        const childWhereClause = prefix + 'type = ' + parts.prefix + 'type' +
+            ' AND ' + prefix + 'hash = ' + parts.prefix + 'hash';
+        const childQuery = this.assembleChildQueryString(
+            { prefix, fromClause, joins, whereClause: childWhereClause }
+        );
+        const whereClause = parts.whereClause +
+            ' AND ' + existence + ' (' + childQuery + ')';
+        return this.stateMatched({
+            prefix: parts.prefix,
+            fromClause: parts.fromClause,
+            joins: parts.joins,
+            whereClause
+        });
     }
 
     private allocateAlias() {
