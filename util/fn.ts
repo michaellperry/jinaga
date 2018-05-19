40908f0a977adab6035e9280328573950ae38994
Michael L Perry <michael@qedcode.com>
2018-05-18 22:37:21 -0500

    Map team members in view model.

--- util/fn.ts
+++ util/fn.ts
@@ -1,12 +1,27 @@
 export async function flattenAsync<T, U>(collection: T[], selector: (element: T) => Promise<U[]>) {
-    const results = await Promise.all(collection.map(selector));
-    return results.reduce((a,b) => a.concat(b));
+    if (collection.length === 0) {
+        return [];
+    }
+    else {
+        const results = await Promise.all(collection.map(selector));
+        return results.reduce((a,b) => a.concat(b));
+    }
 }
 
 export function flatten<T, U>(collection: T[], selector: (element: T) => U[]) {
-    return collection.map(selector).reduce((a,b) => a.concat(b));
+    if (collection.length === 0) {
+        return [];
+    }
+    else {
+        return collection.map(selector).reduce((a,b) => a.concat(b));
+    }
 }
 
 export async function mapAsync<T, U>(collection: T[], action: (element: T) => Promise<U>) {
-    return await Promise.all(collection.map(action));
+    if (collection.length === 0) {
+        return [];
+    }
+    else {
+        return await Promise.all(collection.map(action));
+    }
 }
