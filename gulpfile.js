df829cda6c565fe6238bfd2aafce23534a2781d5
Michael L Perry <michael@qedcode.com>
2018-06-13 08:31:52 -0500

    Compile specifically for test.

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@ var gulp = require('gulp');
 
 var { compileClient } = require('./tasks/client');
 var { compileServer } = require('./tasks/server');
-var { compileTest, runTest } = require('./tasks/test');
+var { compileForTest, compileTest, runTest } = require('./tasks/test');
 
 function clean() {
     return del([ './dist/' ]);
@@ -13,4 +13,4 @@ gulp.task('clean', clean);
 
 gulp.task('default', gulp.parallel([ compileClient, compileServer ]));
 
-gulp.task('test', gulp.series([ compileServer, compileTest, runTest ]));
+gulp.task('test', gulp.series([ compileForTest, compileTest, runTest ]));
--- tasks/test.js
+++ tasks/test.js
@@ -1,25 +1,34 @@
 var gulp = require('gulp');
 var ts = require('gulp-typescript');
 var mocha = require('gulp-mocha');
+var path = require('path');
+
+var config = {
+    module: "commonjs",
+    target: "es6",
+    noImplicitAny: true,
+    moduleResolution: "node",
+    rootDir: "..",
+    baseUrl: "..",
+    lib: [ "dom", "es2015" ],
+    paths: {
+        "*": [
+            path.join(__dirname, "../node_modules/*"),
+            path.join(__dirname, "../types/*")
+        ]
+    }
+};
+
+function compileForTest() {
+    return gulp.src('./src/**/*.ts')
+        .pipe(ts(config))
+        .pipe(gulp.dest('./dist/test/src'));
+}
 
 function compileTest() {
     return gulp.src('./test/**/*.ts')
-        .pipe(ts({
-            module: "commonjs",
-            target: "es6",
-            noImplicitAny: true,
-            moduleResolution: "node",
-            rootDir: "..",
-            baseUrl: "..",
-            lib: [ "dom", "es2015" ],
-            paths: {
-                "*": [
-                    "./node_modules/*",
-                    "./types/*"
-                ]
-            }
-        }))
-        .pipe(gulp.dest('./dist/test'));
+        .pipe(ts(config))
+        .pipe(gulp.dest('./dist/test/test'));
 }
 
 function runTest() {
@@ -30,6 +39,7 @@ function runTest() {
 }
 
 module.exports = {
+    compileForTest,
     compileTest,
     runTest
 };
