f310af3ebc9f329dcd24d332a621bcb763f1621b
Michael L Perry <michael@qedcode.com>
2018-06-12 23:30:37 -0500

    Try to find the code under test.

--- test/parserSpec.ts
+++ test/parserSpec.ts
@@ -1,8 +1,16 @@
 import { expect } from 'chai';
 import { describe, it } from 'mocha';
 
+import { parseQuery } from '../src/query/query-parser';
+
 describe('Addition', () => {
     it('should be correct', () => {
         expect(2+2).to.equal(4);
     })
-})
+});
+
+describe('Query parser', () => {
+    it('should exist', () => {
+        expect(parseQuery).not.to.be.null;
+    })
+});
