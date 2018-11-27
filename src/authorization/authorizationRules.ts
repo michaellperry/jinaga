7b14a4b8477b175aabfffb7873ecbbcab8ea9563
Michael L Perry <michael@qedcode.com>
2018-11-26 21:37:01 -0600

    Collect rules by fact type.

--- src/authorization/authorizationRules.ts
+++ src/authorization/authorizationRules.ts
@@ -1,8 +1,30 @@
 import { UserIdentity } from "../keystore";
 import { FactRecord } from "../storage";
 
+interface AuthorizationRule {
+    isAuthorized(userIdentity: UserIdentity, fact: FactRecord): boolean;
+}
+
+class AuthorizationRuleAny implements AuthorizationRule {
+    isAuthorized(userIdentity: UserIdentity, fact: FactRecord): boolean {
+        return true;
+    }
+}
+
 export class AuthorizationRules {
-    isAuthorized(userIdentity: UserIdentity, facts: FactRecord[]): boolean {
-        return false;
+    private rulesByType: {[type: string]: AuthorizationRule[]} = {};
+
+    type(type: string) {
+        const oldRules = this.rulesByType[type] || [];
+        const newRules = [ ...oldRules, new AuthorizationRuleAny() ];
+        const newRulesByType = { ...this.rulesByType, [type]: newRules };
+        const result = new AuthorizationRules();
+        result.rulesByType = newRulesByType;
+        return result;
+    }
+
+    isAuthorized(userIdentity: UserIdentity, fact: FactRecord): boolean {
+        const rules = this.rulesByType[fact.type] || [];
+        return rules.some(r => r.isAuthorized(userIdentity, fact));
     }
 }
--- test/authorization/authorizationRulesSpec.ts
+++ test/authorization/authorizationRulesSpec.ts
@@ -3,13 +3,41 @@ import { AuthorizationRules } from "../../src/authorization/authorizationRules";
 import { UserIdentity } from "../../src/keystore";
 import { FactRecord } from "../../src/storage";
 
+function givenUserIdentity(): UserIdentity {
+    return null;
+}
+
+function givenFactRecord(): FactRecord {
+    return {
+        type: 'Known',
+        fields: {},
+        hash: '',
+        predecessors: {}
+    };
+}
+
+function givenAuthorizationRules(builder: (a: AuthorizationRules) => AuthorizationRules =
+        a => a): AuthorizationRules {
+    return builder(new AuthorizationRules());
+}
+
 describe('Authorization rules', () => {
-    it('should reject all facts', () => {
-        const authorizationRules = new AuthorizationRules();
-        const userIdentity: UserIdentity = null;
-        const facts: FactRecord[] = [];
-        const authorized = authorizationRules.isAuthorized(userIdentity, facts);
+    it('should reject all facts by default', () => {
+        const authorizationRules = givenAuthorizationRules();
+        const userIdentity = givenUserIdentity();
+        const fact = givenFactRecord();
+        const authorized = authorizationRules.isAuthorized(userIdentity, fact);
 
         expect(authorized).to.be.false;
     });
+
+    it('should accept known facts', () => {
+        const authorizationRules = givenAuthorizationRules(a => a
+            .type('Known'));
+        const userIdentity = givenUserIdentity();
+        const fact = givenFactRecord();
+        const authorized = authorizationRules.isAuthorized(userIdentity, fact);
+
+        expect(authorized).to.be.true;
+    })
 });
