cf4491c0bdb08658117edead331c149000923345
Michael L Perry <michael@qedcode.com>
2019-01-13 11:45:11 -0600

    Extracted service runner to utils.

--- test/service/serviceRunnerSpec.ts
+++ test/service/serviceRunnerSpec.ts
@@ -1,53 +1,5 @@
 import { expect } from "chai";
-
-class ServiceInstance {
-    promise: Promise<void> = null;
-}
-
-class ServiceRunner {
-    private services: ServiceInstance[] = [];
-
-    constructor(
-        private error: (exception: any) => void = _ => { }
-    ) { }
-
-    run(service: () => Promise<void>) {
-        const instance = new ServiceInstance();
-        this.services.push(instance);
-        try {
-            const promise = service()
-                .then(() => {
-                    this.succeed(instance);
-                })
-                .catch(x => {
-                    this.fail(instance, x);
-                });
-            instance.promise = promise;
-        }
-        catch (x) {
-            this.fail(instance, x);
-        }
-    }
-
-    all() {
-        return Promise.all(this.services.filter(s => s.promise).map(s => s.promise));
-    }
-
-    private succeed(instance: ServiceInstance) {
-        this.remove(instance);
-    }
-
-    private fail(instance: ServiceInstance, exception: any) {
-        this.error(exception);
-        this.remove(instance);
-    }
-
-    private remove(instance: ServiceInstance) {
-        const index = this.services.indexOf(instance);
-        if (index >= 0)
-            this.services.splice(index, 1);
-    }
-}
+import { ServiceRunner } from "../../src/util/serviceRunner";
 
 describe('ServiceRunner', () => {
     it('should terminate when no services running', async () => {
