51522e106c80f866ff9a49aff260b90aa0874ed9
Michael L Perry <michael@qedcode.com>
2019-01-14 07:45:31 -0600

    Trace all database hits.

--- src/cache.ts
+++ src/cache.ts
@@ -1,5 +1,6 @@
 import { Query } from './query/query';
 import { FactRecord, FactReference, Storage } from './storage';
+import { Trace } from './util/trace';
 
 export class Cache implements Storage {
     constructor(private inner: Storage) {
@@ -7,18 +8,22 @@ export class Cache implements Storage {
     }
 
     save(facts: FactRecord[]) {
+        Trace.warn(`Save: ${JSON.stringify({facts}, null, 2)}`);
         return this.inner.save(facts);
     }
 
     query(start: FactReference, query: Query) {
+        Trace.warn(`Query: ${JSON.stringify({start, query: query.toDescriptiveString()}, null, 2)}`);
         return this.inner.query(start, query);
     }
 
     exists(fact: FactReference): Promise<boolean> {
+        Trace.warn(`Exists: ${JSON.stringify({fact}, null, 2)}`);
         return this.inner.exists(fact);
     }
 
     load(references: FactReference[]) {
+        Trace.warn(`Load: ${JSON.stringify({references}, null, 2)}`);
         return this.inner.load(references);
     }
 }
