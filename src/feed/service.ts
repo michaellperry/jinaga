c8710c2ecdbd2dfffcb83f672525eff541bf231c
Michael L Perry <michael@qedcode.com>
2019-01-13 18:12:59 -0600

    Fixed hydration bug.

--- src/feed/service.ts
+++ src/feed/service.ts
@@ -1,24 +1,26 @@
-import { hydrate } from '../fact/hydrate';
+import { Hydration } from '../fact/hydrate';
 import { Query } from '../query/query';
-import { FactReference, FactRecord } from '../storage';
-import { ServiceRunner } from '../util/serviceRunner';
+import { FactReference } from '../storage';
 import { mapAsync } from '../util/fn';
+import { ServiceRunner } from '../util/serviceRunner';
 import { Feed } from './feed';
 
 export function runService<U>(feed: Feed, start: FactReference, query: Query, serviceRunner: ServiceRunner, handler: (message: U) => Promise<void>) {
-    let processing: FactRecord[] = [];
-    feed.from(start, query)
+    let processing: FactReference[] = [];
+    const subscription = feed.from(start, query)
         .subscribe((pathsAdded) => {
             const factsAdded = pathsAdded.map(p => p[p.length - 1]);
             serviceRunner.run(async () => {
                 const recordsAdded = await feed.load(factsAdded);
-                await mapAsync(recordsAdded, async (fact) => {
-                    processing.push(fact);
-                    const message = hydrate<U>(fact);
-                    await handler(message);
+                const hydration = new Hydration(recordsAdded);
+                await mapAsync(factsAdded, async reference => {
+                    processing.push(reference);
+                    const fact = <U>hydration.hydrate(reference);
+                    await handler(fact);
                 });
             });
         }, (pathsRemoved) => {
         }
     );
+    return subscription;
 }
--- test/service/serviceSpec.ts
+++ test/service/serviceSpec.ts
@@ -1,5 +1,5 @@
 import { expect } from "chai";
-import { dehydrateReference } from "../../src/fact/hydrate";
+import { dehydrateReference, dehydrateFact } from "../../src/fact/hydrate";
 import { FeedImpl } from "../../src/feed/feed-impl";
 import { runService } from "../../src/feed/service";
 import { MemoryStore } from "../../src/memory/memory-store";
@@ -12,10 +12,16 @@ class TestContext {
     private exceptions: any[] = [];
     private serviceRunner = new ServiceRunner(exception => this.exceptions.push(exception));
 
-    run(fact: {}, queryString: string, handler: (message: {}) => Promise<void>) {
+    async fact(fact: {}) {
+        const records = dehydrateFact(fact);
+        await this.feed.save(records);
+    }
+
+    async run(fact: {}, queryString: string, handler: (message: {}) => Promise<void>) {
         const start = dehydrateReference(fact);
         const query = fromDescriptiveString(queryString);
-        runService(this.feed, start, query, this.serviceRunner, handler);
+        const subscription = runService(this.feed, start, query, this.serviceRunner, handler);
+        await subscription.load();
     }
 
     async stop() {
@@ -31,13 +37,62 @@ describe('Service', () => {
     it('should not run for empty store', async () => {
         const context = new TestContext();
 
-        let runs = 0;
-        context.run({
+        const start = {
             type: 'Start',
             value: 1
-        }, 'S.parent F.type="Child"', async (message) => { ++runs; });
+        };
+        let runs = 0;
+        await context.run(start, 'S.parent F.type="Child"', async _ => { ++runs; });
         await context.stop();
         context.expectNoExceptions();
         expect(runs).to.equal(0);
+    });
+
+    it('should run for existing fact', async () => {
+        const context = new TestContext();
+
+        const start = {
+            type: 'Start',
+            value: 1
+        };
+        await context.fact({
+            type: 'Child',
+            parent: start
+        });
+        let runs = 0;
+        await context.run(start, 'S.parent F.type="Child" N(S.child F.type="Handled")', async child => {
+            await context.fact({
+                type: 'Handled',
+                child: child
+            });
+            ++runs;
+        });
+        await context.stop();
+        context.expectNoExceptions();
+        expect(runs).to.equal(1);
+    });
+
+    it('should run for new fact', async () => {
+        const context = new TestContext();
+
+        const start = {
+            type: 'Start',
+            value: 1
+        };
+        let runs = 0;
+        await context.run(start, 'S.parent F.type="Child" N(S.child F.type="Handled")', async child => {
+            await context.fact({
+                type: 'Handled',
+                child: child
+            });
+            ++runs;
+        });
+        await context.fact({
+            type: 'Child',
+            parent: start
+        });
+        await context.stop();
+        context.expectNoExceptions();
+        expect(runs).to.equal(1);
     });
 });
