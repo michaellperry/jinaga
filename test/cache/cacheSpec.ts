35ef2f8de9761a1aad34fa50c568c48f34a3e2c8
Michael L Perry <michael@qedcode.com>
2019-01-16 12:49:07 -0600

    Test cache of load request.

---
+++ test/cache/cacheSpec.ts
@@ -0,0 +1,71 @@
+import { Cache } from "../../src/cache";
+import { MemoryStore } from "../../src/memory/memory-store";
+import { expect } from "chai";
+import { dehydrateFact, dehydrateReference } from "../../src/fact/hydrate";
+import { Storage, FactRecord, FactReference } from "../../src/storage";
+import { Query } from "../../src/query/query";
+
+class StorageProxy implements Storage {
+    public loadCount = 0;
+
+    constructor(
+        private inner: Storage
+    ) { }
+
+    save(facts: FactRecord[]): Promise<FactRecord[]> {
+        return this.inner.save(facts);
+    }
+    
+    query(start: FactReference, query: Query): Promise<FactReference[][]> {
+        return this.inner.query(start, query);
+    }
+
+    exists(fact: FactReference): Promise<boolean> {
+        return this.inner.exists(fact);
+    }
+
+    load(references: FactReference[]): Promise<FactRecord[]> {
+        this.loadCount++;
+        return this.inner.load(references);
+    }
+}
+
+describe('Cache', () => {
+    function givenStoreWith(records: FactRecord[]) {
+        const memory = new MemoryStore();
+        memory.save(records);
+        const proxy = new StorageProxy(memory);
+        const cache = new Cache(proxy);
+        return { proxy, cache };
+    }
+
+    it('should return empty result', async () => {
+        const { cache } = givenStoreWith([]);
+
+        const results = await cache.load([]);
+        expect(results).to.deep.equal([]);
+    });
+
+    it('should pass first request through to storage', async () => {
+        const fact = { type: 'Root', identifier: 'qedcode' };
+        const records = dehydrateFact(fact);
+        const reference = dehydrateReference(fact);
+        const { cache, proxy } = givenStoreWith(records);
+
+        const results = await cache.load([reference]);
+        expect(results).to.deep.equal(records);
+        expect(proxy.loadCount).to.equal(1);
+    });
+
+    it('should serve second request from cache', async () => {
+        const fact = { type: 'Root', identifier: 'qedcode' };
+        const records = dehydrateFact(fact);
+        const reference = dehydrateReference(fact);
+        const { cache, proxy } = givenStoreWith(records);
+
+        await cache.load([reference]);
+        const results = await cache.load([reference]);
+        expect(results).to.deep.equal(records);
+        expect(proxy.loadCount).to.equal(1);
+    });
+});
