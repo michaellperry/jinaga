55bddc1c9fee93152ca1354c588206c75562a0bb
Michael L Perry <michael@qedcode.com>
2020-06-13 23:22:36 -0500

    Replaced tweetnacl with stablelib.

--- src/util/encoding.ts
+++ src/util/encoding.ts
@@ -1,6 +1,6 @@
 import { encode as encodeBase64 } from '@stablelib/base64';
+import { hash } from '@stablelib/sha512';
 import { encode as encodeUTF8 } from '@stablelib/utf8';
-import { hash } from 'tweetnacl';
 
 export function computeStringHash(str: string) {
   const bytes = encodeUTF8(str);
